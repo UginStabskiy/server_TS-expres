d3299e2e126cfbfc87af08a4ffc415eb6073dc95
Shyabskiy Ugin <mr.shtabskiy@gmail.com>
2022-06-21 18:09:49 +0300

    refactoring ifelsetest

--- dist/ifelsetest.js
+++ dist/ifelsetest.js
@@ -1,6 +1,6 @@
 let num = 44;
 if (num % 2 === 0) {
-    document.write("четне чтсло, як не дивно");
+    document.write("четне чтсло, як не дивно 666");
 }
 else {
     document.write("не четне цисло");
--- src/ifelsetest.ts
+++ src/ifelsetest.ts
@@ -1,6 +1,6 @@
 const num:number = 44
 if (num%2==0){
-    document.write("четне чтсло, як не дивно 777");
+    document.write("четне чтсло, як не дивно 666");
 
 }else{
     document.write("не четне цисло");
