a3b7a96444135c508883cd0cf0ae5c6214ef11ac
Shyabskiy Ugin <mr.shtabskiy@gmail.com>
2022-06-21 18:00:19 +0300

    ifelsetest

---
+++ dist/ifelsetest.js
@@ -0,0 +1,7 @@
+let num = 44;
+if (num % 2 === 0) {
+    document.write("четне чтсло, як не дивно");
+}
+else {
+    document.write("не четне цисло");
+}
---
+++ src/ifelsetest.ts
@@ -0,0 +1,7 @@
+const num:number = 44
+if (num%2==0){
+    document.write("четне чтсло, як не дивно 777");
+
+}else{
+    document.write("не четне цисло");
+}
