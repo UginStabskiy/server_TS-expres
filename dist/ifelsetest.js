ad68e5f15558d39f155852cea0b6e8ba68a87920
Shyabskiy Ugin <mr.shtabskiy@gmail.com>
2022-06-22 11:49:13 +0300

    refactoring tsconfig.json (lerning)

--- dist/ifelsetest.js
+++ dist/ifelsetest.js
@@ -1,4 +1,5 @@
-let num = 44;
+"use strict";
+const num = 44;
 if (num % 2 === 0) {
     document.write("четне чтсло, як не дивно 666");
 }
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,6 @@ import express from "express"
 const app = express();
 const port = 5000;
 app.get('/', (request, response) =>{
-    response.send('Hello world!');
+    response.send('Hello world!9999');
 });
 app.listen(port, () => console.log(`Running on port ${port}`));
