d40b9c5e503df5cc3f4865df86ea16ee74488bb8
Shyabskiy Ugin <mr.shtabskiy@gmail.com>
2022-06-23 17:56:33 +0300

    refactoring Simple typse.ts Simple typse.html

---
+++ dist/Simple typse.js
@@ -0,0 +1,16 @@
+"use strict";
+let x = "Всяке різне";
+document.write(x);
+let x1 = 475;
+console.log(x1); // як вивести результат?
+let x2 = false;
+console.log(x2); // як вивести данні у термінал?
+const myScore = 10;
+console.log(myScore);
+const myScore1 = "10";
+document.write(myScore1);
+let x3 = JSON.parse("6");
+console.log(x3);
+let isOdd;
+let y = 22;
+isOdd = y % 2 !== 0;
